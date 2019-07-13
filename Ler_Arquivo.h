f632f13ca873e05c523c2b3ce294c212ef55b97c
Rafael Farias <51766474+rvfarias@users.noreply.github.com>
2019-07-12 21:02:06 -0300

    Delete Ler_Arquivo.h

--- Ler_Arquivo.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LER_ARQUIVO_H__
-#define __LER_ARQUIVO_H__
-
-void LerArqCasa();                               // Ler aquivo de imoveis tipo Casa
-void LerArqApartamento();                        // Ler aquivo de tipo Apartamento
-void LerArqTerreno();                            // Ler aquivo de tipo Terreno
-
-#endif
