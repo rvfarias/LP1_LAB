591ac010c8908862e2e1769b47b2e411ba691a0e
Rafael Farias <51766474+rvfarias@users.noreply.github.com>
2019-07-12 21:05:47 -0300

    Delete cadastrar.h

--- cadastrar.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CADASTRAR_H__
-#define __CADASTRAR_H__
-
-  void CadastraCasa(int Num_Imov);                               // Cadastra imoveis tipo Casa
-  void CadastraApartamento(int Num_Imov);                        // Cadastra imoveis tipo Apartamento
-  void CadastraTerreno(int Num_Imov);                            // Cadastra imoveis tipo Terreno
-
-#endif
